7277ec5389d98b49a5011f476d12e471670b3f3d
Jonathan Sherman <jonmsherman08@gmail.com>
2019-03-18 21:13:37 -0500

    Added 512 channels with sliders

---
+++ dmx-app/dmx-app/Channel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace dmxapp
+{
+    public class Channel
+    {
+        public int Index { get; set; }
+        public int Value { get; set; }
+
+        public Channel()
+        {
+        }
+    }
+}
