c00b6c18246b533939a49f1359f9a2ea121218e5
Jonathan Sherman <jonmsherman08@gmail.com>
2019-03-18 20:52:14 -0500

    added macOS support

---
+++ dmx-app.macos/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿using AppKit;
+using dmx_app;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace dmxapp.macos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow _window;
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1025, 768);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "Xamarin.Forms on Mac!";
+            _window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+
+        }
+
+        public override NSWindow MainWindow
+        {
+            get { return _window; }
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+            // Insert code here to initialize your application
+        }
+
+    }
+}
---
+++ dmx-app.macos/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace dmxapp.macos
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+        }
+    }
+}
