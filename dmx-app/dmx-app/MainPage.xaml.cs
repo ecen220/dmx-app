df1c2c546cf3730cb53e21ee2c210779ec65ac96
Jonathan Sherman <jonmsherman08@gmail.com>
2019-03-18 21:13:37 -0500

    Added 512 channels with sliders

--- dmx-app/dmx-app/MainPage.xaml.cs
+++ dmx-app/dmx-app/MainPage.xaml.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dmxapp;
 using Xamarin.Forms;
 
 namespace dmx_app
 {
     public partial class MainPage : ContentPage
     {
+        public List<Channel> Channels = new List<Channel>();
+
         public MainPage()
         {
+            for (int i = 0; i < 512; i++)
+            {
+                Channels.Add(new Channel()
+                {
+                    Index = i,
+                    Value = 0
+                });
+            }
+
+
             InitializeComponent();
+
+            channels.ItemsSource = Channels;
         }
     }
 }
