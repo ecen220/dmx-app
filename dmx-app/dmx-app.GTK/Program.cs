e701018508df76ceb9e7e390a1a9bb9da9333788
Jonathan Sherman <jonmsherman08@gmail.com>
2019-03-18 23:56:49 -0500

    Added Linux support

---
+++ dmx-app/dmx-app.GTK/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using dmx_app;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace dmxapp.GTK
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("DMX app");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
