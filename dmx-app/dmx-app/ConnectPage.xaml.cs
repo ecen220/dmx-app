a55c0de77bff82a16a9b259af298336c35952ffe
Jonathan Sherman <jonmsherman08@gmail.com>
2019-04-02 16:07:17 -0500

    Added a connect screen, fixed the slider UI issue for MacOSX and set a constant window width and height

---
+++ dmx-app/dmx-app/ConnectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace dmxapp
+{
+    public partial class ConnectPage : ContentPage
+    {
+        public ConnectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
